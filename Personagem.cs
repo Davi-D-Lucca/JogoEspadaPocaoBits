5ab512da79dbcbd5f291c09092ab6ff107c37b2a
Davi-D-Lucca <ddlucca10@gmail.com>
2026-04-17 11:45:03 -0300

    Criacao classe personagem

---
+++ Personagem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JogoEspadaPocaoBits
+{
+    class Personagem
+    {
+        public string Nome { get; set; }
+        public int Vida { get; set; }
+        public bool Defender { get; set; }
+
+        public void ReceberDano(int dano)
+        {
+            if(Defender == true)
+            {
+                dano = dano / 2;
+            }
+
+            Vida = Vida - dano;
+
+            if(Vida < 0)
+            {
+                Vida = 0;
+            }
+        }
+
+        public void Curar(int valorCura)
+        {
+            Vida = Vida + valorCura;
+            if(Vida > 100)
+            {
+                Vida = 100;
+            }
+        }
+
+        public bool EstaVivo()
+        {
+            return Vida > 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JogoEspadaPocaoBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
