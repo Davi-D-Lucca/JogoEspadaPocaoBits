8d48a02c1e1635fa6f2d75a452d77f2d96f21d2f
Davi-D-Lucca <ddlucca10@gmail.com>
2026-04-17 12:09:03 -0300

    Criando classe inimigo

---
+++ Inimigo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JogoEspadaPocaoBits
+{
+    class Inimigo : Personagem
+    {
+        private Random random = new Random();
+        public int EscolherAcao()
+        {
+            int acao = random.Next(1, 4);
+            return acao;
+        }
+    }
+}
