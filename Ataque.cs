9edef567ad50c61cb376d2e7593e21ffc6456b1e
Davi-D-Lucca <ddlucca10@gmail.com>
2026-04-17 12:24:15 -0300

    Criando classe ataque

---
+++ Ataque.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JogoEspadaPocaoBits
+{
+    class Ataque
+    {
+        public enum TipoAtaque
+        {
+            Leve,
+            Medio,
+            Forte
+        }
+
+        private Random random = new Random();
+
+        public TipoAtaque Tipo { get; set; }
+        public int DanoMin { get; set; }
+        public int DanoMax { get; set; }
+        public int ChanceAcerto { get; set; }
+
+        public int CalcularDano()
+        {
+            int chance = random.Next(1, 1001);
+            if(chance <= ChanceAcerto)
+            {
+                var dano = random.Next(DanoMin, DanoMax + 1);
+                return dano;
+            }
+            return 0;
+        }
+    }
+}
